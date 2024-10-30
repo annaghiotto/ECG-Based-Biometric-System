cbcd9d6a102dff9c052e0a3c91d3474914ba7d62
Adrian Maciej Osedowski <adrian.osedowski@students.salle.url.edu>
2024-10-30 12:53:40 +0000

    data source fix

--- DataSource.py
+++ DataSource.py
@@ -4,64 +4,68 @@ import numpy as np
 from abc import abstractmethod, ABC
 from dataclasses import dataclass
 
+
 @dataclass
 class Instance:
-    signal: np.ndarray
+    signals: []
     id: int
 
 
 class DataSource(ABC):
     def __init__(self, filename):
-        self.filename = filename
-        self.filelist = []
         self.person = 1
-        self.record = 1
+        self.filename = filename
 
     def __iter__(self):
         return self
-    
+
     def __next__(self) -> Instance:
         pass
-    
+
 
 class GetSBData(DataSource):
 
-    def __iter__(self):
+    def __init__(self, filename):
+        super().__init__(filename)
+        self.person_signals = {}
         filelist = iter(os.listdir(os.fsencode(self.filename)))
-        self.person = 0
         for filename in filelist:
-            self.filelist.append(os.fsdecode(filename))
-        return self
+            filename = self.filename + '/' + os.fsdecode(filename)
+            print(filename)
+            signal = np.loadtxt(filename)
+            person = int(filename.split('_')[4].split('u')[1].split('.')[0])
+            try:
+                self.person_signals[person].append(signal)
+            except:
+                self.person_signals[person] = [signal]
 
     def __next__(self) -> Instance:
-        try:    
-            filename = self.filename + '/' + self.filelist[self.person]
-            print(filename)
-            signals = np.loadtxt(filename)
-            person = int(filename.split('_')[4].split('u')[1].split('.')[0])
-            self.person += 1
-            return signals, person
-        except IndexError:
-            raise StopIteration
-    
-class GetEcgIDData(DataSource):
-    
-    def __next__(self) -> Instance:
-        filename = self.filename + '/Person_' + f"{self.person:02}" + '/rec_' + str(self.record)
         try:
-            signals, fields = wfdb.rdsamp(filename)
-            self.record += 1
-            return signals, self.person
-        except FileNotFoundError:
+            person = self.person
             self.person += 1
-            self.record = 1
-            filename = self.filename + '/Person_' + f"{self.person:02}" + '/rec_' + str(self.record)
-            try:
-                signals, fields = wfdb.rdsamp(filename)
-                self.record += 1
-                return signals, self.person
-            except FileNotFoundError:
-                raise StopIteration
+            return self.person_signals[person], person
+        except KeyError:
+            raise StopIteration
+
+
+class GetEcgIDData(DataSource):
+
+    def __next__(self) -> Instance:
+        record = 1
+        person_signals = []
+        if os.path.exists(self.filename + '/Person_' + f"{self.person:02}"):
+            while True:
+                try:
+                    filename = self.filename + '/Person_' + f"{self.person:02}" + '/rec_' + str(record)
+                    signal, fields = wfdb.rdsamp(filename)
+                    person_signals.append(signal)
+                    record += 1
+                except FileNotFoundError:
+                    person = self.person
+                    self.person += 1
+                    return person_signals, person
+        else:
+            raise StopIteration
 
 
 data = GetEcgIDData('ecg-id-database-1.0.0')
